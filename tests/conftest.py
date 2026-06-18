ac09306ea11079df73070a3e6c59f74744671ded
ElliotWhy <elliot@cineon.ai>
2026-06-18 11:21:52 +0100

    Update tests to use a crossformer fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 from cineon_format import CineonData
 from pytest import fixture
 
+from example_crossformer_sdk import CrossformerModel
+
 
 @fixture(scope="session")
 def data() -> CineonData:
     return CineonData.from_csv("data/example.csv")
+
+@fixture(scope="session")
+def crossformer() -> CrossformerModel:
+    return CrossformerModel("model")
